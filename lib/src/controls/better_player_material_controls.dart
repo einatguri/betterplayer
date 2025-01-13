56d7089be2dea6c77bbd3ab761ca62ca2ec4d392
Einat Kidron <einatkidron24@gmail.com>
2025-01-13 11:00:48 +0200

    fixed controls hide time hard coded

--- lib/src/controls/better_player_material_controls.dart
+++ lib/src/controls/better_player_material_controls.dart
@@ -672,7 +672,7 @@ class _BetterPlayerMaterialControlsState
     if (_betterPlayerController!.controlsAlwaysVisible) {
       return;
     }
-    _hideTimer = Timer(const Duration(milliseconds: 3000), () {
+    _hideTimer = Timer(_controlsConfiguration.controlsHideTime, () {
       changePlayerControlsNotVisible(true);
     });
   }
